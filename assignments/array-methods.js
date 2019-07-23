868d253bd2a08bc436fcb04213f172c7520db2b5
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-07-23 15:46:44 -0700

    Finished

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -69,19 +69,20 @@ let allCaps = runners.map( function(names){
 });
 
 console.log(allCaps); 
-// let namesAndEmails = runners.map( function(arrayItem){
-//     return { first_name: arrayItem.first_name, last_name: arrayItem.last_name, email: arrayItem.email}
-//   })
-// console.log(namesAndEmails);
+
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(function(arrayItem){
+  return arrayItem.shirt_size === "L";
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function (accum,don){
+ return accum + don.donation;
+});
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
